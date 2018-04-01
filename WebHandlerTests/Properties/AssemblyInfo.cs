ff3e98b471eac29af9400550a011086ef00ba636
sdemalet123 <sdemalet@gmail.com>
2018-04-01 21:11:11 +0100

    Added some unit testing

---
+++ WebHandlerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebHandlerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebHandlerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91c3303f-0c3f-4d7e-b3a4-2f4715a3b8f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebHandlerTests/WebHandlerControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebDataHandler.Services;
+using NSubstitute;
+using WebDataHandler.Models;
+using WebDataHandler.Controllers;
+using Shouldly;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace WebHandlerTests
+{
+    [TestClass]
+    public class WebHandlerControllerTests
+    {
+        [TestMethod]
+        public void Test_GetPropertyData_Success()
+        {
+            var webDataService = Substitute.For<IWebDataService>();
+            var property = new Property {
+                Bathrooms = 1,
+                Beds = 2,
+                Guests = 4,
+                PropertyType = "studio",
+                Url = "myUrl" };
+            webDataService.GetWebData("myUrl").Returns(property);
+
+            var controller = new WebDataController(webDataService);
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var response = controller.GetPropertyData("myUrl");
+
+            response.Bathrooms.ShouldBe(1);
+            response.Beds.ShouldBe(2);
+            response.Guests.ShouldBe(4);
+            response.PropertyType.ShouldBe("studio");
+            response.Url.ShouldBe("myUrl");
+
+       }
+    }
+}
